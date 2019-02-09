a29f00201bf9c05cfbcfb21ff91a401ba32f3797
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-02-09 04:52:06 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,10 @@
 class Fish
   # code goes here
+    attr_accessor :name
+  attr_reader :mood 
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
   # code goes here
+  Owners = []
+
+  attr_accessor :pets, :name
+  attr_reader :species
+
+  def self.all
+    Owners
+  end
+
+  def self.count
+    Owners.size
+  end
+
+  def self.reset_all
+    Owners.clear
+end
 end
